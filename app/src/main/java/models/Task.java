71efc5440e7475d40e39b1b104b30a9eb1007b1e
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2021-03-24 10:38:02 +0100

    Writing javadoc comments and adding equals and hashCode methods

--- app/src/main/java/models/Task.java
+++ app/src/main/java/models/Task.java
@@ -3,73 +3,157 @@ package models;
 import utilities.Priority;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
+/*
+* This is  is base class which handle the information about the task.
+* This task allows the application to assign values for all the task.
+*
+* @version 1.0
+* @since  1.0
+* */
 public class Task {
+  /** Represents the description of the task.
+  */
   private String description;
+  /** Represtns the task's priority
+  */
   private Priority priority;
+  /** Represents the task's category.
+  */
   private String category;
+  /*Represents the task's startedDate.
+  */
   private LocalDate startedDate;
+  /**Represents the task's finishedDate.
+  */
   private LocalDate finishedDate;
+  /** Represents the task's deadline.
+  */
   private LocalDate deadline;
+  /** Represents the task's title.
+  */
   private String title;
-
+/** task's constructor
+ * Creates a task with the specific title
+ * @param title representing The task's title
+*/
   public Task(String title) {
     this.setTitle(title);
   }
-
+/**Gets the task's description.
+ * @return a string representing task's description.
+*/
   public String getDescription() {
     return description;
   }
-
+/**Sets the task's description.
+ * @param description- String.
+*/
   public void setDescription(String description) {
     this.description = description;
   }
-
+/**Gets the task's priority.
+ * @return a Priority representing priority.
+*/
   public Priority getPriority() {
     return priority;
   }
-
+/**Sets the task's priority.
+ * @param priority-Priority.
+*/
   public void setPriority(Priority priority) {
     this.priority = priority;
   }
-
+/** Gets the task's category.
+ * @return A Category representing task's category.
+*/
   public String getCategory() {
     return category;
   }
-
+/**Sets the task's category.
+ * @param category-String.
+*/
   public void setCategory(String category) {
     this.category = category;
   }
-
+/** Gets The task's startDate
+ * @return localDate representing the task's startedDate.
+*/
   public LocalDate getStartedDate() {
     return startedDate;
   }
-
+/**Sets the task's startedDate
+ * @param startedDate-localDate.
+*/
   public void setStartedDate(LocalDate startedDate) {
     this.startedDate = startedDate;
   }
-
+/**Gets task's finishedDate.
+ * @return localDate representing the task's finishedDate.
+*/
   public LocalDate getFinishedDate() {
     return finishedDate;
   }
-
+/**Sets' The task's finishedDate.
+ * @param finishedDate -localDate.
+*/
   public void setFinishedDate(LocalDate finishedDate) {
     this.finishedDate = finishedDate;
   }
-
+/** Gets task's deadline.
+ * @return A localDate representing the task's deadline.
+*/
   public LocalDate getDeadline() {
     return deadline;
   }
 
+  /**Sets the task's deadline.
+   * @param deadline .
+   */
   public void setDeadline(LocalDate deadline) {
     this.deadline = deadline;
   }
 
+  /**Gets the task's title.
+   * @return A String representing the task's title.
+   */
   public String getTitle() {
     return title;
   }
 
+  /**Sets the task's title.
+   * @param title-String.
+   */
   public void setTitle(String title) {
     this.title = title;
+  }
+
+  /**This is  method which check the equality of tasks.
+   * @param object-an Object.
+   * @return A boolean true if the tasks are equal, or false if not.
+   */
+
+  @Override
+  public boolean equals(Object object) {
+    boolean result;
+    if (this == object) {
+      result = true;
+    } else if (!(object instanceof Task)) {
+      result = false;
+    } else {
+      Task task = (Task) object;
+      result = getPriority() == task.getPriority() && Objects.equals(getCategory(), task.getCategory()) && Objects.equals(getStartedDate(), task.getStartedDate()) && Objects.equals(getFinishedDate(), task.getFinishedDate()) && Objects.equals(getDeadline(), task.getDeadline()) && Objects.equals(getTitle(), task.getTitle());
+    }
+    return result;
+  }
+
+  /**Returns the hash code of a non-null argument and 0 for a null argument.
+   * hashCode in class Object.
+   * @return hash code value for this object.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash( getPriority(), getCategory(), getStartedDate(), getFinishedDate(), getDeadline(), getTitle());
   }
 }
