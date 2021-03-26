02a9dfb3d8c57adb80995b75d455cdd64f67d73f
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2021-03-26 23:01:59 +0100

    Writing two methods 'getTasks and getTasksbyStatus for class TaskRegistry

--- app/src/main/java/models/TaskRegistry.java
+++ app/src/main/java/models/TaskRegistry.java
@@ -1,5 +1,7 @@
 package models;
 
+import dao.PersistentRegistry;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import utilities.Priority;
 import utilities.Status;
@@ -8,19 +10,45 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javafx.collections.FXCollections.observableArrayList;
+
 public class TaskRegistry {
   private List<Task> tasks;
-
+  PersistentRegistry fileHandle;
+  private final String fileName="TasksData.dat";
+  private Status status;
+  private Priority priority;
+/*
+*
+*
+* */
   public TaskRegistry() {
     this.tasks = new ArrayList<Task>();
+    fileHandle=new PersistentRegistry();
   }
-
+/*
+*
+* */
   public ObservableList<Task> getTasks() {
-    return null;
+    //Object tasks=new ArrayList<>();
+    tasks=fileHandle.loadDate(fileName);
+    ObservableList<Task> allTasks= observableArrayList(tasks);
+    return allTasks;
   }
+  /*
+  *
+  *
+  * */
 
-  public ObservableList<Task> getTasksByStatus(Status status) {
-    return null;
+  public ObservableList<Task> getTasksByStatus(String status) {
+    ArrayList<Task> foundTaskByStatus = new ArrayList<>();
+    tasks =  fileHandle.loadDate(fileName);
+    for ( Task task :  tasks)
+      if (task.getStatus().equals(status)) {
+        foundTaskByStatus.add(task);
+      }
+    ObservableList<Task>AllFoundTasks= FXCollections.observableArrayList(foundTaskByStatus);
+    return AllFoundTasks;
   }
 
   public ObservableList<Task> getTasksByDate(LocalDate fromDate, LocalDate toDate) {
