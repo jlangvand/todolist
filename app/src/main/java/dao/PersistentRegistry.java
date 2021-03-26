964a713b3ae786bb79d2f19536f80e488f9b0e2e
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2021-03-26 22:38:12 +0100

    Writign methods in PersientRegistry to help in writing methods for class TaskRegistry

--- app/src/main/java/dao/PersistentRegistry.java
+++ app/src/main/java/dao/PersistentRegistry.java
@@ -1,8 +1,14 @@
 package dao;
 
+import models.Task;
 import models.TaskRegistry;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersistentRegistry {
+ // Task task=new Task();
   public PersistentRegistry(String fileName) {
   }
 
@@ -12,4 +18,33 @@ public class PersistentRegistry {
 
   public void save() {
   }
+  /*
+  * It is just for helping to write the functions in taskRegistry class
+  * We can delete or changing after that.
+  * */
+
+  public void saveData(String fileName, Task task) throws IOException {
+    FileOutputStream fileOut=new FileOutputStream("fileName");
+    ObjectOutputStream out=new ObjectOutputStream(fileOut);
+    out.writeObject(task);
+    out.close();
+    fileOut.close();
+  }
+
+  public List<Task> loadDate(String fileName)  {
+    List<Task> deserialized = new ArrayList<>();
+    try{
+      FileInputStream fileIn = new FileInputStream("fileName");
+      ObjectInputStream in=new ObjectInputStream(fileIn);
+      deserialized=(ArrayList<Task>) in.readObject();
+    }catch (IOException ex){
+      System.out.println("Error: "+ex.getMessage());
+    } catch(Exception ex){
+      ex.getMessage();
+    }
+
+    return  deserialized;
+
+  }
+
 }
