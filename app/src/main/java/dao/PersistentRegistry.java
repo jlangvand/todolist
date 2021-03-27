654607d502e68ea5602b4076aa5e7c7c615c3a00
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2021-03-27 20:50:41 +0100

    Modifying PersistentRegistry Class

--- app/src/main/java/dao/PersistentRegistry.java
+++ app/src/main/java/dao/PersistentRegistry.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersistentRegistry {
- // Task task=new Task();
+  File file;
   public PersistentRegistry(String fileName) {
   }
 
@@ -27,24 +27,34 @@ public class PersistentRegistry {
   * We can delete or changing after that.
   * */
 
-  public void saveData(String fileName, Task task) throws IOException {
-    FileOutputStream fileOut=new FileOutputStream("fileName");
-    ObjectOutputStream out=new ObjectOutputStream(fileOut);
-    out.writeObject(task);
-    out.close();
-    fileOut.close();
+  public void saveData(String fileName, Task task) throws Exception {
+      file=new File(fileName);
+      try {
+          FileOutputStream fileOut = new FileOutputStream(file);
+          ObjectOutputStream out = new ObjectOutputStream(fileOut);
+          out.writeObject(task);
+          out.close();
+          fileOut.close();
+      }catch(IOException ex){
+          ex.printStackTrace();
+      }catch(Exception ex){
+          ex.printStackTrace();
+      }
   }
 
   public List<Task> loadDate(String fileName)  {
     List<Task> deserialized = new ArrayList<>();
+    file=new File(fileName);
     try{
-      FileInputStream fileIn = new FileInputStream("fileName");
+      FileInputStream fileIn = new FileInputStream(file);
       ObjectInputStream in=new ObjectInputStream(fileIn);
       deserialized=(ArrayList<Task>) in.readObject();
     }catch (IOException ex){
       System.out.println("Error: "+ex.getMessage());
+    } catch(ClassNotFoundException ex){
+        System.out.println("Error: "+ex.getMessage());
     } catch(Exception ex){
-      ex.getMessage();
+        System.out.println("Error: " +ex.getMessage());
     }
 
     return  deserialized;
