3f54e7b04c3a4893b734e964c60fd78698af843d
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2021-03-28 22:57:15 +0200

    Creating class PersistentRegistryTest

--- app/src/test/test/dao/PersistentRegistryTest.java
+++ app/src/test/test/dao/PersistentRegistryTest.java
@@ -1,4 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package dao;
+
+import models.Task;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import utilities.Priority;
+import utilities.Status;
+
+import java.time.LocalDate;
+
 class PersistentRegistryTest {
-  
+    Task task=new Task();
+
+    @Before
+    public void setUp() throws Exception{
+        task.setTitle("Meeting a friend");
+        task.setDescription("I will meet him at restaurant ");
+        task.setPriority(Priority.LOW);
+        task.setCategory("social");
+        task.setStartedDate(LocalDate.of(2021,5,14));
+        task.setFinishedDate(LocalDate.of(2021,5,14));
+        task.setDeadline(LocalDate.of(2021,5,14));
+        task.setStatus(Status.ACTIVE);
+    }
+
+
+    @Test
+    void saveData() {
+
+    }
+
+    @Test
+    void loadDate() {
+
+    }
 }
