7b8cacea7e421140784f2c02104a5f2c03083313
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-03-29 19:11:52 +0200

    Remove unused methods

--- app/src/main/java/dao/PersistentRegistry.java
+++ app/src/main/java/dao/PersistentRegistry.java
@@ -46,44 +46,4 @@ public class PersistentRegistry {
       oos.writeObject(this.tasks);
     }
   }
-
-  /*
-   * It is just for helping to write the functions in taskRegistry class
-   * We can delete or changing after that.
-   * */
-
-  public void saveData(String fileName, Task task) throws Exception {
-    file = new File(fileName);
-    try {
-          FileOutputStream fileOut = new FileOutputStream(file);
-          ObjectOutputStream out = new ObjectOutputStream(fileOut);
-          out.writeObject(task);
-          out.close();
-          fileOut.close();
-      }catch(IOException ex){
-          ex.printStackTrace();
-      }catch(Exception ex){
-          ex.printStackTrace();
-      }
-  }
-
-  public List<Task> loadDate(String fileName)  {
-    List<Task> deserialized = new ArrayList<>();
-    file=new File(fileName);
-    try{
-      FileInputStream fileIn = new FileInputStream(file);
-      ObjectInputStream in=new ObjectInputStream(fileIn);
-      deserialized=(ArrayList<Task>) in.readObject();
-    }catch (IOException ex){
-      System.out.println("Error: "+ex.getMessage());
-    } catch(ClassNotFoundException ex){
-        System.out.println("Error: "+ex.getMessage());
-    } catch(Exception ex){
-        System.out.println("Error: " +ex.getMessage());
-    }
-
-    return  deserialized;
-
-  }
-
 }
