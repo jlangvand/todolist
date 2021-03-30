719ffab635ef2fc572e9b13cbee04cb41dd7b27c
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-03-30 14:37:53 +0200

    Added navigationBar controller and code to test setting allTasks as center of navigation bar.

---
+++ app/src/main/java/controllers/NavBarController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavBarController implements Initializable {
+    @FXML
+    BorderPane pane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //Temporary, to test loading of allTasks as center of navigationBar.
+        try {
+            pane.setCenter(FXMLLoader.load(new File("app/src/main/resources/view/allTasks.fxml").toURI().toURL()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
