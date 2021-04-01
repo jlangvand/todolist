bd2b75bb704d6ed421bad5bad000b7a0914f9ce6
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-04-01 14:59:27 +0200

    Added variable for priority combo box.

--- app/src/main/java/controllers/EditTaskController.java
+++ app/src/main/java/controllers/EditTaskController.java
@@ -1,6 +1,11 @@
 package controllers;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTimePicker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
@@ -14,27 +19,27 @@ public class EditTaskController {
     @FXML
     private JFXTextField nameField;
 
-    @FXML
-    private JFXTextArea descriptionField;
+  @FXML
+  private JFXTextArea descriptionField;
 
-    @FXML
-    private JFXDatePicker deadlineDateField;
+  @FXML
+  private JFXDatePicker deadlineDateField;
 
-    @FXML
-    private JFXTimePicker deadlineTimeField;
+  @FXML
+  private JFXTimePicker deadlineTimeField;
+
+  @FXML
+  private JFXComboBox<?> priorityComboBox;
+
+  @FXML
+  private JFXTextField categoryField;
 
 
-    @FXML
-    private JFXTextField categoryField;
+  @FXML
+  private JFXButton saveEdit;
 
-    @FXML
-    private JFXComboBox<?> priorityComboBox;
-
-    @FXML
-    private JFXButton saveEdit;
-
-    @FXML
-    private JFXButton cancelEdit;
+  @FXML
+  private JFXButton cancelEdit;
 
     private Task task;
 
