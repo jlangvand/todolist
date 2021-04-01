9fe3e28cf91795231533ac6e33ecebcfc680d108
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-04-01 05:01:55 +0200

    Added controller skeleton for navigation bar.

--- app/src/main/java/controllers/NavBarController.java
+++ app/src/main/java/controllers/NavBarController.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 
 import java.io.File;
@@ -14,14 +16,96 @@ public class NavBarController implements Initializable {
     @FXML
     BorderPane pane;
 
+    @FXML
+    private JFXButton allTasksButton;
+
+    @FXML
+    private JFXButton highPriorityButton;
+
+    @FXML
+    private JFXButton mediumPriorityButton;
+
+    @FXML
+    private JFXButton lowPriorityButton;
+
+    @FXML
+    private JFXButton trashButton;
+
+    /**
+     * This button is called when the navBarController is initialized.
+     *
+     * @param location
+     * @param resources
+     */
+    @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        //Temporary, to test loading of allTasks as center of navigationBar.
         try {
-            pane.setCenter(FXMLLoader.load(new File("src/main/resources/view/allTasks.fxml").toURI().toURL()));
+            updateCenter("allTasks");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+    /**
+     * This method is called when allTasksButton is clicked.
+     *
+     * @param event
+     */
+    @FXML
+    void displayAllTasks(MouseEvent event) throws IOException {
+        updateCenter("allTasks");
+
+    }
+
+
+    /**
+     * This method is called when highPriorityButton is clicked.
+     *
+     * @param event
+     */
+    @FXML
+    void displayHighPriorityTasks(MouseEvent event) {
+
+    }
+
+
+    /**
+     * This method is called when mediumPriorityButton is clicked.
+     *
+     * @param event
+     */
+    @FXML
+    void displayMediumPriorityTasks(MouseEvent event) {
+
+    }
+
+
+    /**
+     * This method is called when lowPriorityButton is clicked.
+     *
+     * @param event
+     */
+    @FXML
+    void displayLowPriorityTasks(MouseEvent event) {
+
+    }
+
+
+    /**
+     * This method is called when trashButton is clicked.
+     *
+     * @param event
+     */
+    @FXML
+    void displayTrash(MouseEvent event) {
+
+    }
+
+    private void updateCenter(String fxml) throws IOException {
+        URL url = new File("src/main/resources/view/" + fxml + ".fxml").toURI().toURL();
+        pane.setCenter(FXMLLoader.load(url));
+    }
+
 }
