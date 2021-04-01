bb2a99c345e1b0c220080b3aa4fc3c8eb7697488
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-04-01 19:40:33 +0200

    Change test method, fix formatting

--- app/src/test/java/models/TaskRegistryTest.java
+++ app/src/test/java/models/TaskRegistryTest.java
@@ -4,76 +4,72 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TaskRegistryTest {
-    private TaskRegistry taskRegistry;
+  private TaskRegistry taskRegistry;
 
 
+  @BeforeEach
+  void setUp() throws IOException {
 
-    @BeforeEach
-    void setUp() throws IOException {
+  }
 
-    }
+  @Test
+  void getTasksTest() throws IOException {
+    taskRegistry = new TaskRegistry("test2.bin");
+    Task task1 = new Task("Meeting a friend");
+    Task task2 = new Task("Reading a book");
+    taskRegistry.addTask(task1);
+    taskRegistry.addTask(task2);
+    assertTrue(taskRegistry.getTasks().contains(task1));
+    assertTrue(taskRegistry.getTasks().contains(task2));
+  }
 
-    @Test
-    void getTasksTest() throws IOException {
-        ArrayList<Task>tasks=new ArrayList<>();
-        taskRegistry=new TaskRegistry("test2.bin");
-        Task task1=new Task("Meeting a friend");
-        Task task2=new Task("Reading a book");
-        taskRegistry.addTask(task1);
-        taskRegistry.addTask(task2);
+  @Test
+  void getTasksByStatus() {
+  }
 
-        assertEquals(task1,taskRegistry.getTasks().get(0));
-        assertEquals(task2,taskRegistry.getTasks().get(1));
-    }
+  @Test
+  void getTasksByPriority() {
+  }
 
-    @Test
-    void getTasksByStatus() {
-    }
+  @Test
+  void getTasksByDate() {
+  }
 
-    @Test
-    void getTasksByPriority() {
-    }
+  @Test
+  void testGetTasksByDate() {
+  }
 
-    @Test
-    void getTasksByDate() {
-    }
+  @Test
+  void getHighPriorityTasks() {
+  }
 
-    @Test
-    void testGetTasksByDate() {
-    }
+  @Test
+  void getMediumPriorityTasks() {
+  }
 
-    @Test
-    void getHighPriorityTasks() {
-    }
+  @Test
+  void getLowPriorityTasks() {
+  }
 
-    @Test
-    void getMediumPriorityTasks() {
-    }
+  @Test
+  void getDoneTasks() {
+  }
 
-    @Test
-    void getLowPriorityTasks() {
-    }
+  @Test
+  void getActiveTasks() {
+  }
 
-    @Test
-    void getDoneTasks() {
-    }
-
-    @Test
-    void getActiveTasks() {
-    }
-
-    @Test
-    void addTask() throws IOException {
+  @Test
+  void addTask() throws IOException {
 
 
-    }
+  }
 
-    @Test
-    void removeTask() {
-    }
+  @Test
+  void removeTask() {
+  }
 }
