1ae5cf701a0e71cb7061bd0d7d3bf23cf72174c2
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-04-02 12:58:36 +0200

    Add default priority and toString overrides

--- app/src/main/java/utilities/Priority.java
+++ app/src/main/java/utilities/Priority.java
@@ -1,7 +1,28 @@
 package utilities;
 
 public enum Priority {
-  HIGH,
-  MEDIUM,
-  LOW
+  HIGH {
+    @Override
+    public String toString() {
+      return "High";
+    }
+  },
+  MEDIUM {
+    @Override
+    public String toString() {
+      return "Medium";
+    }
+  },
+  LOW {
+    @Override
+    public String toString() {
+      return "Low";
+    }
+  },
+  DEFAULT {
+    @Override
+    public String toString() {
+      return "Not set";
+    }
+  }
 }
