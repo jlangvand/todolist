8822de81ce2e9d80893a762cc499e4e0e90410ba
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-04-08 10:48:24 +0200

    DRY code, move FXML loader to utility class

---
+++ app/src/main/java/utilities/Utilities.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Utilities {
+  private Utilities() {
+
+  }
+
+  /**
+   * Loads an fxml file from the path app/src/main/resources/view
+   *
+   * @param fxml Filename of fxml, without the file extension (.fxml)
+   * @return Parent object representing the view
+   * @throws IOException exception is thrown if file I/O fails
+   */
+  public static FXMLLoader getFXMLLoader(String fxml) throws IOException {
+    return new FXMLLoader(
+        new File("src/main/resources/view/" + fxml + ".fxml")
+            .toURI().toURL());
+  }
+
+}
