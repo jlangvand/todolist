2a264bcd224bb014ac4af692a656ec3e9b55e5b6
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-04-08 12:57:36 +0200

    Cleaner code

--- app/src/main/java/utilities/Priority.java
+++ app/src/main/java/utilities/Priority.java
@@ -1,28 +1,19 @@
 package utilities;
 
 public enum Priority {
-  HIGH {
-    @Override
-    public String toString() {
-      return "High";
-    }
-  },
-  MEDIUM {
-    @Override
-    public String toString() {
-      return "Medium";
-    }
-  },
-  LOW {
-    @Override
-    public String toString() {
-      return "Low";
-    }
-  },
-  DEFAULT {
-    @Override
-    public String toString() {
-      return "Not set";
-    }
+  HIGH("High"),
+  MEDIUM("Medium"),
+  LOW("Low"),
+  DEFAULT("Not set");
+
+  private String description;
+
+  Priority(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return description;
   }
 }
