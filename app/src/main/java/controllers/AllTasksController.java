5c5de038d00055f0e446eb9b51606a55a74e5d12
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-04-08 13:27:34 +0200

    Code cleanup

--- app/src/main/java/controllers/AllTasksController.java
+++ app/src/main/java/controllers/AllTasksController.java
@@ -1,31 +1,18 @@
 package controllers;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXDecorator;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import models.Task;
 import models.TaskRegistry;
-import utilities.Priority;
-import utilities.Status;
 
-import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 public class AllTasksController {
 
@@ -63,7 +50,6 @@ public class AllTasksController {
     private ObservableList<Task> tasks;
     private NavBarController navBarController;
 
-
     @FXML
     void initialize() {
     }
@@ -92,20 +78,8 @@ public class AllTasksController {
   @FXML
   void displayTask(MouseEvent event) throws IOException {
       if (!allTasksList.getSelectionModel().isEmpty()) {
-          Task selectedTask = allTasksList.getSelectionModel().getSelectedItem();
-/*          allTasksList.getSelectionModel().clearSelection();
-
-          FXMLLoader fxmlLoader = new FXMLLoader(new File("src/main/resources/view/ViewTask.fxml").toURI().toURL());
-          Parent root = fxmlLoader.load();
-          ViewTaskController viewTaskController = fxmlLoader.getController();
-          viewTaskController.initData(selectedTask);
-
-          Stage stage = new Stage();
-          JFXDecorator decorator = new JFXDecorator(stage, root);
-          decorator.setCustomMaximize(true);
-          stage.setScene(new Scene(decorator));
-          stage.initModality(Modality.APPLICATION_MODAL);
-          stage.show();*/
+          Task selectedTask = allTasksList.getSelectionModel()
+              .getSelectedItem();
           navBarController.loadDisplayTaskView(selectedTask);
       }
   }
