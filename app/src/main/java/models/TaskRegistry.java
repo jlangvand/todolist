df63a97ad92245da245b5c3863c8aa7aa4a81a22
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-04-10 18:36:58 +0200

    Implemented method for switching tasks

--- app/src/main/java/models/TaskRegistry.java
+++ app/src/main/java/models/TaskRegistry.java
@@ -186,6 +186,20 @@ public class TaskRegistry extends ArrayList<Task> implements Serializable {
     save();
   }
 
+  /**
+   * Swaps the index of the task of index a with the task of index b.
+   * @param a Index of task A
+   * @param b Index of task B
+   * @throws IOException throws exception if file IO fails
+   */
+  public void swapTasksByIndex(int a, int b) throws IOException {
+    Task taskA = get(a);
+    Task taskB = get(b);
+    set(a,taskB);
+    set(b, taskA);
+    save();
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
