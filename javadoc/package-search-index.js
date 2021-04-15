5bbeb04212d02ce33464d476255f8ce4cd492d5a
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2021-04-15 09:52:50 +0200

    javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"todolist.app.main","l":"controllers"},{"m":"todolist.app.main","l":"dao"},{"m":"todolist.app.main","l":"models"},{"m":"todolist.app.main","l":"todolist.app"},{"m":"todolist.app.main","l":"utilities"}]
